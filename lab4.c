f154ee909ba37c43df86136163c732306a7b0dde
Darron Yi <dyi@riggs309lab03.cecas.clemson.edu>
2025-02-26 12:35:55 -0500

    main read

--- lab4.c
+++ lab4.c
@@ -1,7 +1,19 @@
 #include <stdio.h>
 
 int main(int argv, char *argc[]){
-  printf("\nhello there\n");
-  
-  return 0;
+  if(argc != 4){
+    fprintf(stderr, "Usage: %s compress|decompress inputfile outputfile\n", argv[0]);
+    return EXIT_FAILURE;
+  }
+  if(strcmp(argv[1], "c") == 0){
+    compress(argv[2], argv[3]);
+  }
+  else if(strcmp(argv[1], "d") == 0){
+    decompress(argv[2], argv[3]);
+  }
+  else{
+    fprintf(stderr, "Invalid command. Use 'compress' or 'decompress'.\n");
+    return EXIT_FAILURE;
+  }
+  return EXIT_SUCCESS;
 }
