d79c50b2052783409ebabcf197fe490e3a86643d
Jai Agarwal <jaiveer.agarwal@gmail.com>
2025-03-04 23:47:41 -0500

    use -c and -d flags to compress or decompress. writes table of freq/data to compressed file

--- lab4.c
+++ lab4.c
@@ -1,3 +1,13 @@
+/*
+ * Jai Agarawl & Darron Yi
+ * ECE 4680 - Huffman Codec
+ * 
+ * This file implements Huffman encoding and decoding.
+ *
+ * compile with: 'gcc lab4.c -o huffman'
+ * run with:     './huffman -c [file]' to create [file].huf    (compressed)
+ *               './huffman -d [file].huf' to create [file].d  (decompressed)
+ */
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
@@ -37,28 +47,6 @@ int isLeafNode (Node *node) {
   return (node->left == NULL && node->right == NULL);
 }
 
-// create new node
-Node* newNode(char data, unsigned int freq) {
-  Node* new = malloc(sizeof(Node));
-
-  new->data = data;
-  new->freq = freq;
-  new->left = NULL;
-  new->right = NULL;
-
-  return new;
-}
-
-Tree* createTree(unsigned int capacity) {
-  struct Tree* newTree = malloc(sizeof(Tree));
-  newTree->capacity = capacity;
-  newTree->size = 0;
-
-  newTree->root = NULL;
-
-  return newTree;
-}
-
 // Less than zero (<0): If the first argument should be placed before the second argument.
 // Zero (0): If both arguments are equal.
 // Greater than zero (>0): If the first argument should be placed after the second argument.
@@ -79,6 +67,39 @@ int compare_freq(const void *a, const void *b) {
   return -1;
 
   return 0; // equal nodes
+}
+
+// sorts passed data in ascending order
+// returns pointer to node array
+Node **sort_ascending(Queue q, int size) {
+  qsort(q.array, size, sizeof(Node *), compare_freq);
+  return q.array;
+}
+
+void print_inorder(Node *node);
+
+void printLevelOrder(Node *root, int size);
+
+// create new node
+Node* newNode(char data, unsigned int freq) {
+  Node* new = malloc(sizeof(Node));
+
+  new->data = data;
+  new->freq = freq;
+  new->left = NULL;
+  new->right = NULL;
+
+  return new;
+}
+
+Tree* createTree(unsigned int capacity) {
+  Tree* newTree = malloc(sizeof(Tree));
+  newTree->capacity = capacity;
+  newTree->size = 0;
+
+  newTree->root = NULL;
+
+  return newTree;
 }
 
 Queue createQueue(char data[], unsigned int freq[], int size) {
@@ -102,52 +123,6 @@ Queue createQueue(char data[], unsigned int freq[], int size) {
   return q;
 }
 
-void fix_size(Tree *T)
-{
-  T->size = 1;
-  if (T->root->left)
-  T->size++;
-  if (T->root->right)
-  T->size++;
-}
-
-// insert key k into tree T, returning a pointer to the resulting root
-Node *insert(Node *T, char data, unsigned int freq) {
-  if (T == NULL) return newNode(data, freq);
-  if (freq < T->freq)
-  T->left = insert(T->left, data, freq);
-  else
-  T->right = insert(T->right, data, freq);
-  return T;
-}
-
-// join trees L and R (with L containing keys all <= the keys in R)
-// args: two pointers to Nodes of subtrees
-// returns: a pointer to the joined tree.
-Node *join(Node *L, Node *R)
-{
-  // |L|/(|L|+|R|) & |R|/(|L|+|R|)
-  //Implement Node *join(Node *L, Node *R)
-  if (L == NULL)
-  return R;
-  if (R == NULL)
-  return L;
-  Node *newRoot;
-  // right root becomes root
-  if (R->left == NULL)
-  R->left = L;
-  else R->left = join(L, R->left);
-  newRoot = R;
-  return newRoot;
-}
-
-// sorts passed data in ascending order
-// returns pointer to node array
-Node **sort_ascending(Queue q, int size) {
-  qsort(q.array, size, sizeof(Node *), compare_freq);
-  return q.array;
-}
-
 // gets and removes the front from the queue
 Node *dequeue(Queue *q) {
   if (q->size == 0) {
@@ -165,9 +140,6 @@ Node *dequeue(Queue *q) {
 
   // printf("dequeued: %d:%c\n", min->freq, min->data);
 
-  // re-sort
-  qsort(q->array, q->size, sizeof(Node *), compare_freq);
-
   return min;
 }
 
@@ -177,9 +149,6 @@ void insertQueue(Queue *q, Node* node) {
 
   q->array[q->size] = node;
   q->size++;
-
-
-  // Re-sort the queue
 
   // printf("after inserting: %d:%c\n", node->freq, node->data);
   // for (int i = 0; i < q->size; i++) {
@@ -236,17 +205,88 @@ void encode(Node *root, char arr[], int top, char codes[256][100]) {
     encode(root->right, arr, top + 1, codes);
   }
 
-  // if node is a leaf node then print enpty the buffered code into the file
+  // if node is a leaf node then print the buffered code into the file
   if (isLeafNode(root)) {
     arr[top] = '\0';
-    strcpy(codes[(unsigned char)root->data], arr);
+    strcpy(codes[(unsigned char)root->data], arr); // copy generated code into 'codes' array
   }
+}
+
+void writeHeaderToFile(char *outputFile, char data[], int freq[], int size) {
+  FILE* output = fopen(outputFile, "wb");
+  char str[50];
+
+  // write size of the table first, with a space
+  sprintf(str, "%d " , size);
+  fwrite(str, 1, strlen(str), output);
+  
+  // write elements of the table
+  // writes int followed by char
+  for(int i = 0; i < size; i++) {
+    sprintf(str, "%d:%c", freq[i], data[i]);
+    fwrite(str, 1, strlen(str), output);
+  }
+  fclose(output);
+}
+
+// parses header of a compressed file, create tree based on freq data read in
+// return root node of tree
+long parseHeaderBuildTree(char *inputFile, Node **root) {
+  FILE* input = fopen(inputFile, "r");
+  if (!input) {
+    printf("Error opening input file\n");
+    return -1;
+  }
+
+  char in;
+  int size = 0;
+  long position; // record file offset;
+
+  // get size of table
+  char terminator = ' ';
+  char buffer[128];
+  int index = 0;
+  while ((in = fgetc(input)) != EOF) { // Read character by character
+    if (in == terminator) {
+      break;  // Stop when the terminator is found
+    }
+    buffer[index++] = in;
+    buffer[index] = '\0';  // Null-terminate each step
+  }
+  size = atoi(buffer);
+
+  int freq[size];
+  char data[size];
+
+  terminator = ':';
+  // read size num of char and int pairs
+  // read int(fre1) followed by char(data)
+  for (int i = 0; i < size; i++) {
+    index = 0;
+    while ((in = fgetc(input)) != EOF) { // Read character by character
+      if (in == terminator) {
+        break;  // Stop when the terminator is found
+      }
+      buffer[index++] = in;
+      buffer[index] = '\0';  // Null-terminate each step
+    }
+    freq[i] = atoi(buffer);
+    fread(&data[i], sizeof(char), 1, input);
+  }
+  position = ftell(input);
+  fclose(input);
+
+  *root = buildTree(data, freq, size);
+  printf("tree of size: %d\n", size);
+  printLevelOrder(*root, size);
+
+  return position;
 }
 
 // function to take the codes and insert them into an output file
 void compress(const char *inputFile, const char *outputFile, char codes[256][100]){
-  FILE* input = fopen(inputFile, "r");
-  FILE* output = fopen(outputFile, "wb");
+  FILE* input = fopen(inputFile, "rb");
+  FILE* output = fopen(outputFile, "ab");
   unsigned char buffer = 0;
   int bitCount = 0;
   char ch;
@@ -270,12 +310,14 @@ void compress(const char *inputFile, const char *outputFile, char codes[256][100
   fclose(output);
 }
 
-// function to take the tree
-void decompress(struct Node* root, const char* encodedFile, const char* decodedFile) {
+// function to take the tree root, read from a file, and decode using the tree
+void decompress(Node* root, char* encodedFile, char* decodedFile, long file_offset) {
   FILE* input = fopen(encodedFile, "rb");
-  FILE* output = fopen(decodedFile, "w");
-  struct Node* curr = root;
+  FILE* output = fopen(decodedFile, "wb");
+  Node* curr = root;
   unsigned char buffer;
+
+  fseek(input, file_offset, SEEK_SET); // seek to after the header
   while (fread(&buffer, 1, 1, input)) {
     for (int i = 7; i >= 0; i--) {
       curr = ((buffer >> i) & 1) ? curr->right : curr->left;
@@ -320,73 +362,118 @@ void printLevelOrder(Node *root, int size) {
   q.capacity = size;
   q.array = arr;
 
+  int tier = 0;
   // Enqueue the root node
   insertQueue(&q, root);
   printf("\n");
   while (q.size != 0) {
+    // pad with spaces based on what tier this is
+    for (int i = 0; i < 10 - tier; i++) printf("     ");
+  
     // Get the current level size
     int levelSize = q.size;
     // Process all nodes at the current level
-    for (int i = 0; i < levelSize; ++i) {
+    for (int i = 0; i < levelSize; i++) {
       Node *node = dequeue(&q);
-      printf("%d:%c    ", node->freq, node->data);
-
+      printf("%d:%c", node->freq, node->data);  
+      for (int j = 0; j < 7 - levelSize; j++) printf(" ");
+      
       // Enqueue left child if it exists
       if (node->left != NULL)
-      insertQueue(&q, node->left);
+        insertQueue(&q, node->left);
 
       // Enqueue right child if it exists
       if (node->right != NULL)
-      insertQueue(&q, node->right);
+        insertQueue(&q, node->right);
     }
     // Print new line after each level is processed
     printf("\n");
+    tier++;
   }
 }
 
 int main(int argc, char *argv[]){
-  if (argc != 4) {
-    printf("Usage: %s <input file> <encoded file> <decoded file>\n", argv[0]);
+  if (argc != 3) {
+    printf("Usage: %s -c|-d <input file>\n", argv[0]);
     return 1;
   }
 
   int freq[256] = {0};
-  FILE* input = fopen(argv[1], "r");
-  if (!input) {
-    printf("Error opening input file\n");
-    return 1;
-  }
-  // Read entire input file
-  char ch;
-  while ((ch = fgetc(input)) != EOF){
-    freq[(unsigned char)ch]++;
-  }
+  char *inputFile = argv[2];
+  char *outputFile = (char *)malloc(sizeof(argv[2] + 6));
+  
+  Tree *tree;
+  FILE* input;
 
-  fclose(input);
+  /****************** encode *****************/
+  if (strcmp(argv[1], "-c") == 0) {
+    // setting outputFile
+    strcpy(outputFile, inputFile);
+    strcat(outputFile, ".huff");
 
-  // Extracts data and stores their corresponding frequency
-  char data[256];
-  int freqArray[256], size = 0;
-  for (int i = 0; i < 256; i++) {
-    if (freq[i] > 0) {
-      data[size] = (char)i;
-      freqArray[size] = freq[i];
-      size++;
+    input = fopen(inputFile, "rb");
+    if (!input) {
+      printf("Error opening input file\n");
+      return 1;
     }
+  
+    // read entire input file one byte ate a time, generating freqs
+    char ch;
+    while ((ch = fgetc(input)) != EOF){
+      freq[(unsigned char)ch]++;
+    }
+
+    fclose(input);
+
+    // extracts data and stores their corresponding frequency
+    char data[256];
+    int freqArray[256], size = 0;
+    for (int i = 0; i < 256; i++) {
+      if (freq[i] > 0) {
+        data[size] = (char)i;
+        freqArray[size] = freq[i];
+        size++;
+      }
+    }
+
+    // create and build the tree
+    tree = createTree(size);
+    tree->root = buildTree(data, freqArray, size);
+
+    /* printing out */
+    printf("tree of size: %d\n",size);
+    print_inorder(tree->root);
+    printf("\n\n");
+    printf("%d:%c\n", tree->root->freq, tree->root->data);
+    printLevelOrder(tree->root, tree->size);
+    /* end printing out */
+
+    // generate codes from tree
+    char arr[100], top = 0;
+    char codes[256][100] = {0};
+    encode(tree->root, arr, top, codes);
+    
+    // write header to file and then write compressed data
+    writeHeaderToFile(outputFile, data, freqArray, size);
+    compress(inputFile, outputFile, codes);
   }
+  /********************* decode *********************/
+  else if (strcmp(argv[1], "-d") == 0) { 
+    // setting outputFile
+    strncpy(outputFile, inputFile, strlen(inputFile)-5);
+    strcat(outputFile, ".dec");
 
-  Tree *tree = createTree(sizeof(data)/sizeof(data[0]));
-  printf("%d\n",size);
-  tree->root = buildTree(data, freqArray, size);
-  print_inorder(tree->root);
-  printf("\n\n");
-  printf("%d:%c\n", tree->root->freq, tree->root->data);
+    // parse the header to get freqs and create the tree
+    Node *root = NULL;
+    long file_offset = parseHeaderBuildTree(inputFile, &root);
+    if (file_offset < 0) {
+      printf("error reading header of file\n");
+      return 1;
+    }
+        
+    // decompress and write to file
+    decompress(root, inputFile, outputFile, file_offset);
 
-  char arr[100], top = 0;
-  char codes[256][100] = {0};
-  encode(tree->root, arr, top, codes);
-  compress(argv[1], argv[2], codes);
-  decompress(tree->root, argv[2], argv[3]);
-
+  }
   return 0;
 }
